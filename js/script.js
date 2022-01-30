198975e45a10e72d40fe6d1baef9e694a0e8d019
aivvas <vassi.aiva@gmail.com>
2022-01-30 11:52:55 +0100

    queastion 1- 5

---
+++ js/script.js
@@ -0,0 +1,42 @@
+///Question 1
+Object.property = {
+    name: "cat",
+}
+
+complain.property = [
+    function complain (){
+        console.log("meow!")
+    }
+]
+
+///Question 2
+
+var h3 = document.querySelector ("h3")
+
+function changeheading (){
+    var h3 = document.querySelector ("h3");
+    h3.innerHTML = "Updated heading"
+}
+
+//Question 3
+
+function changefont (){
+    var h3 = document.querySelector ("h3");
+    h3.style.fontSize = "2em"
+}
+
+//Question 4
+document.querySelector ("heading").classList.add("subheading");
+
+//Question 5
+document.querySelectorAll ("p")
+var p = document.querySelectorAll ("p");
+
+
+var p = document.querySelectorAll ("p")
+for (i= 0; i < p.lenght; i++){
+    p.style.backgroundColor = "red"
+};
+
+
+//Question 6
